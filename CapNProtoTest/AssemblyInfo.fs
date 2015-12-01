b0b285ea6f522ebf252f328478664de62c441a27
caleb <caleb@beyondgames.co>
2015-12-01 12:18:33 -0600

    adding project for capnproto unit test

---
+++ CapNProtoTest/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module CapNProtoTest.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("CapNProtoTest")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("caleb")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
